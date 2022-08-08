3114f3009c3a5fbef8437a910f57de0242b11b45
Rohit Kamble <kamble.rohit10792@gmail.com>
2022-08-08 09:40:51 +0530

    Welcome to Sum of three Int adds to zero

---
+++ FunctionalPrograms.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.functionalprograms;
+
+public class FunctionalPrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Sum of three Integer adds to ZERO");
+
+    }
+}
